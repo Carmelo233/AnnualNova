ddc340bead09233bccb02380164d3929a4bd4a7a
Jtwosir <2149833608@qq.com>
2024-03-01 19:14:31 +0800

    大致确定好代码层次

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import {login} from './pages/Login'
+
+function App() {
+    return (
+        <div className="App">
+            login:<login/>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import background from '../asset/LS.page.background.png'
+
+const myStyle = {
+    backgroundImage: `url(${background})`,
+    height: "100vh",
+    marginTop: "-70px",
+    fontSize: "50px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+};
+
+function login() {
+    return {
+    }
+}
+
+export default login;
